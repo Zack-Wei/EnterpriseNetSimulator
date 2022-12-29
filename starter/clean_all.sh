bb554c4476c6f9ded397b89d2efccc19eba9c920
Zack-Wei <datongwei@foxmail.com>
2022-12-29 13:14:56 +0000

    v1.0

---
+++ starter/clean_all.sh
@@ -0,0 +1,10 @@
+
+#this file aims to start all the hosts in order
+
+lclean -d ./internet_zone
+lclean -d ./external_dmz_zone
+lclean -d ./enterprise_zone
+lclean -d ./extranet_zone
+lclean -d ./internal_dmz_zone
+lclean -d ./restricted_zone
+lclean -d ./management_zone
---
+++ starter/crash_all_hosts.sh
@@ -0,0 +1,9 @@
+#this file aims to start all the hosts in order
+
+lcrash -d ./internet_zone
+lcrash -d ./external_dmz
+lcrash -d ./enterprise_zone
+lcrash -d ./extranet_zone
+lcrash -d ./internal_dmz
+lcrash -d ./restricted_zone
+lcrash -d ./management_zone
